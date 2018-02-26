5d634fde7f4b9e441bef9d0cbf3aee9f6caeea55
Heena Patel <heenap98@gmail.com>
2018-02-26 13:04:16 -0500

    Fixed broken login and register

--- app/src/main/java/com/example/heenapatel/m4/LoginActivity.java
+++ app/src/main/java/com/example/heenapatel/m4/LoginActivity.java
@@ -74,7 +74,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         }
         if (registeredUser) {
-            readSDFile();
+//            readSDFile();
             startActivity(new Intent(this, MainActivity.class));
         } else {
             invalidText.setVisibility(View.VISIBLE);
@@ -82,45 +82,45 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
-    private void readSDFile() {
-        SimpleModel model = SimpleModel.INSTANCE;
-
-        try {
-            //Open a stream on the raw file
-            InputStream is = getResources().openRawResource(R.raw.homeless_shelter_database);
-            //From here we probably should call a model method and pass the InputStream
-            //Wrap it in a BufferedReader so that we get the readLine() method
-            BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
-
-            String line;
-            br.readLine(); //get rid of header line
-
-            int count = 0;
-
-            while ((line = br.readLine()) != null) {
-                for (int i = 0; i < line.length(); i++) {
-                    if (line.charAt(i) == ',' && count % 2 == 0) {
-                        line = line.substring(0, i) + ";" + line.substring(i + 1);
-                    }
-                    if (line.charAt(i) == '\"') {
-                        count++;
-                    }
-                }
-
-                String[] tokens = line.split(";");
-                for(int i = 0; i < tokens.length; i++) {
-                    System.out.println(tokens[i]);
-                }
-                System.out.println(tokens.length);
-                int key = Integer.parseInt(tokens[0]);
-                int longitude = Integer.parseInt(tokens[4]);
-                int latitude = Integer.parseInt(tokens[5]);
-                model.addItem(new DataItem(key, tokens[1], tokens[2], tokens[3], longitude, latitude, tokens[6], tokens[7], tokens[8]));
-            }
-            br.close();
-        } catch (IOException e) {
-        }
-
-    }
+//    private void readSDFile() {
+//        SimpleModel model = SimpleModel.INSTANCE;
+//
+//        try {
+//            //Open a stream on the raw file
+//            InputStream is = getResources().openRawResource(R.raw.homeless_shelter_database);
+//            //From here we probably should call a model method and pass the InputStream
+//            //Wrap it in a BufferedReader so that we get the readLine() method
+//            BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+//
+//            String line;
+//            br.readLine(); //get rid of header line
+//
+//            int count = 0;
+//
+//            while ((line = br.readLine()) != null) {
+//                for (int i = 0; i < line.length(); i++) {
+//                    if (line.charAt(i) == ',' && count % 2 == 0) {
+//                        line = line.substring(0, i) + ";" + line.substring(i + 1);
+//                    }
+//                    if (line.charAt(i) == '\"') {
+//                        count++;
+//                    }
+//                }
+//
+//                String[] tokens = line.split(";");
+//                for(int i = 0; i < tokens.length; i++) {
+//                    System.out.println(tokens[i]);
+//                }
+//                System.out.println(tokens.length);
+//                int key = Integer.parseInt(tokens[0]);
+//                int longitude = Integer.parseInt(tokens[4]);
+//                int latitude = Integer.parseInt(tokens[5]);
+//                model.addItem(new DataItem(key, tokens[1], tokens[2], tokens[3], longitude, latitude, tokens[6], tokens[7], tokens[8]));
+//            }
+//            br.close();
+//        } catch (IOException e) {
+//        }
+//
+//    }
 
 }
