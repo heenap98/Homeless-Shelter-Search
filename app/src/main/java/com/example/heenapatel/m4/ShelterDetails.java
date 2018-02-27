fb2ae2163cd7262b5d862cc51c0388928e666c04
Arslan Munaf <arslanmunaf99@gmail.com>
2018-02-27 09:10:46 -0500

    ShelterDetailsBroken
    does not work, but it is a start (before compiler died)

---
+++ app/src/main/java/com/example/heenapatel/m4/ShelterDetails.java
@@ -0,0 +1,36 @@
+package com.example.heenapatel.m4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.support.v7.widget.LinearLayoutManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+public class ShelterDetails extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shelterdetails);
+        List<DataItem> shelterHolder = SimpleModel.INSTANCE.getItems();
+        System.out.println(shelterHolder.get(0));
+
+
+    }
+
+
+
+}
